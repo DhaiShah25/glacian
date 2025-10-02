a9fc732f80d725ad628759ea836468693967e9f6
Dhairya Shah <dhaishah7@gmail.com>
2025-10-02 13:03:26 -0400

    Updated README & simplify shell.nix

--- shell.nix
+++ shell.nix
@@ -1,20 +1,16 @@
 let
   pkgs = import <nixpkgs> {};
-  unstable = import <nixos-unstable> {config = {allowUnfree = true;};};
 in
   pkgs.mkShell {
-    buildInputs = [
-      pkgs.sdl3
-      pkgs.vulkan-validation-layers
-      pkgs.vulkan-loader
-      pkgs.shader-slang
-      pkgs.vulkan-tools-lunarg
-      unstable.rustc
-      unstable.cargo
-      unstable.rust-analyzer
-      unstable.clippy
-      unstable.rustfmt
-      pkgs.vulkan-tools
+    buildInputs = with pkgs; [
+      sdl3
+      vulkan-validation-layers
+      vulkan-loader
+      shader-slang
+      vulkan-tools-lunarg
+      vulkan-tools
+      cargo-zigbuild
+      cmake
     ];
 
     LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [
